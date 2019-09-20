731224b80414b963e37c5f0b667c1c3e7a739987
Hilton Santos <jhilton930@gmail.com>
2019-09-19 21:52:47 -0300

    Add Exercicio-02

---
+++ POO-Exercicio-02/problemas.java
@@ -0,0 +1,46 @@
+
+public class problemas {
+    public static void main(String[] args) {
+        /*int i;
+        double d, d2;
+        String s = 5;
+        d = 3.1415;
+        i = d;
+        i = 3.14;
+        d = 5;
+        i = d;
+
+
+        i = 5;
+        d2 = i;*/
+
+        //int a = 274;
+        //long b = a;
+
+        /*long a = 274;
+        int b = (int) a;
+        double a = 18.7;*/
+
+        //int a = (int)18.7;
+        //System.out.println(b);
+
+        /*int x,y;
+        y = 0;
+        x = 0;
+
+        System.out.println(x++);
+        System.out.println(++y);
+        System.out.println(x);*/
+
+        int a = 10;
+        int b =0;
+        if (a > 10 && (++b) == 0){
+            System.out.println(b);
+        }else{
+            System.out.println(b);
+        }
+
+
+    }
+    
+}
