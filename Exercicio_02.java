40729789310752b91ff4b268787e3ae3701f707c
Hilton Santos <39769638+jhiltonsantos@users.noreply.github.com>
2019-09-24 16:08:04 -0300

    Add Exercicio_02

---
+++ Exercicio_02.java
@@ -0,0 +1,18 @@
+/* Questao 01
+    javac Avaliacao.java - p/compilar
+    jav Avaliacao - p/executar
+*/
+
+/* Questao 02
+   Vai da error no compilador, porque o atributo contador não foi inicializado
+
+*/
+
+/* Questao 03
+    Somente a primeira condição do if (a>11) sera acessada
+    Dando como resultado (a-b)= 9
+*/
+
+// Questao 04
+
+
